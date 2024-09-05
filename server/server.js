46e187cad3c662917c99112866e084f29c6bbc54
anushkaj-01 <122741286+anushkaj-01@users.noreply.github.com>
2024-09-05 06:30:50 +0530

    added models and controllers

--- server/server.js
+++ server/server.js
@@ -3,11 +3,27 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const userRoute = require("./routes/userRoute");
 const app = express();
+const errorHandler = require("./middleware/errorMiddleware");
+const cookieParser = require("cookie-parser");
+//middlewares
+app.use(express.json());
+app.use(cookieParser);
+app.use(express.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cors());
 
+//routes Middleware
+app.use("/api/users", userRoute);
+
+//routes
+app.get("/",(req, res)=>{
+    res.send("home");
+});
 const PORT = process.env.PORT || 5000;
-
+//Error
+app.use(errorHandler);
 //connect to db and start server
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>{
